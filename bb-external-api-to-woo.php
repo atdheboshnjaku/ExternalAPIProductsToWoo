1848e02d68f2af0684cbc8425ae166981c7dcb7c
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2022-02-18 13:01:47 +0100

    Chore: cleanup composer.json and bb-external-api-to-woo.php

--- bb-external-api-to-woo.php
+++ bb-external-api-to-woo.php
@@ -15,3 +15,19 @@
 
  if(!defined('ABSPATH')) { exit; }
 
+//require __DIR__ . '/vendor/autoload.php';
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+use Automattic\Woocommerce\Client;
+
+$woocommerce = new Client(
+
+    'http://plugin.blu/',
+    'ck_d6610f99aa925f4e17b4de4928d41922c221bd89',
+    'cs_de5ed05763b598f485b9496913ccb9a5fff9aa61',
+    [
+        'wp_api'  => true,
+        'version' => 'wc/v3'
+    ]
+
+);
