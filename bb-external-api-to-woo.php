ff65c6d84797f2892cd21b3e424c8835f6523604
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2022-02-18 11:51:06 +0100

    Chore: Setup composer and autoloading

---
+++ bb-external-api-to-woo.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: External API Products To Woocommerce
+ * Description: Simple plugin that automatically downloads FakeStoreapi products from their API and adds them to woocommerce.
+ * Plugin URI: https://wordpress.org
+ * Author: Atdhe Boshnjaku
+ * Version: 1.0
+ * Author URI: https://wordpress.org
+ *
+ * Text Domain: bb-external-api-to-woo
+ *
+ * Simple plugin that automatically downloads FakeStoreapi products from their API and adds them to woocommerce.
+ */
+
+ if(!defined('ABSPATH')) { exit; }
+
